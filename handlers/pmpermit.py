ca05250536f9697ce96b66d1e25b0bcc47c9638f
Daniele Zito <danielezito@consultant.com>
2021-08-24 14:26:17 +0200

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Ciao, io sono solo un povero robot!")
+  await USER.send_message(message.chat.id,"Ciao, io sono solo un povero robot!\n\n Contatta il mio padrone: @Winged_z")
   return                        
